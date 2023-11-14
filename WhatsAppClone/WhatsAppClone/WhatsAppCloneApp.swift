fd2402234c774b66079a1f04415748b295e6df19
Firdavs <firdavsitaly@gmail.com>
2023-11-14 12:25:53 +0100

    Initial commit

---
+++ WhatsAppClone/WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Firdavs Juraev on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
